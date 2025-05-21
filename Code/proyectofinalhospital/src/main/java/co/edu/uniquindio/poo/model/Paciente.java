aa64e0ebd0916fd321297ddc2ab7a8c60dcedf14
JohnDehiby25 <dehibymorales@gmail.com>
2025-05-20 20:11:09 -0400

    Creacion de la clase cita medica y paciente, se realizo la clase Cita medica para poder terminar de concretar los metodos necesarios de administrador, y en la clase paciente se llevo a cabo la realizacion de metodos importantes como el de cancelar citas medicas obtener citasmedicasnuevas y notificar cita ya que son importantes en caso de no poder los pacietnes a asistir a ellas

--- Code/proyectofinalhospital/src/main/java/co/edu/uniquindio/poo/model/Paciente.java
+++ Code/proyectofinalhospital/src/main/java/co/edu/uniquindio/poo/model/Paciente.java
@@ -1,7 +1,7 @@
 package co.edu.uniquindio.poo.model;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.time.LocalDateTime;
 
 public class Paciente extends Usuario{
 	
@@ -51,5 +51,51 @@ public class Paciente extends Usuario{
 		System.out.println("Sus datos fueron actualizados correctamente");
 		
 		actualizacionDatos.close();
+	}
+    public ArrayList<CitaMedica> solicitarCitasMedicas(){
+		ArrayList<CitaMedica> listaCitasMedicasSolicitadas = new ArrayList<>();
+		for(int i=0; i<listCitasMedicas.size();i++) {
+			listaCitasMedicasSolicitadas.add(listCitasMedicas.get(i));
+		}
+		return listaCitasMedicasSolicitadas;
+		
+	}	
+	public ArrayList<CitaMedica> cancelarCitasMedicas(LocalDateTime fechaCitaEliminar){
+		for(int i=0;i<listCitasMedicas.size();i++) {
+			if(listCitasMedicas.get(i).getHorario().isEqual(fechaCitaEliminar)) {
+				listCitasMedicas.remove(i);
+			}
+		}
+		return listCitasMedicas;
+		
+	}
+	
+	public ArrayList<CitaMedica> obtenerCitasMedicasNuevas(Administrador admin){
+		ArrayList<CitaMedica> listCitasMedicas=new ArrayList<>();
+		for(CitaMedica c: admin.getListCitasMedicas()) {
+			if(c.getIdPaciente().equals(c.getIdPaciente())) {
+				listCitasMedicas.add(c);
+			}
+		}
+		return listCitasMedicas;
+	}
+	public HistorialMedico consultarHistorialMedico(LocalDateTime fechabuscar){
+		for(int i=0; i<listHistorialMedico.size();i++) {
+			if(listHistorialMedico.get(i).getFecha().equals(fechabuscar)) {
+				return listHistorialMedico.get(i);
+			}
+		}
+		return null;
+		
+	}
+	public void notificarCita(LocalDateTime fechabuscar) {
+		for(CitaMedica c:listCitasMedicas) {
+			if(c.getHorario().isBefore(fechabuscar)){
+				System.out.println("La fecha de su cita medica se esta acercando");
+			}
+		}
+	}
+	public ArrayList<HistorialMedico> getListHistorialmedico(){
+		return listHistorialMedico;
 	}
 }

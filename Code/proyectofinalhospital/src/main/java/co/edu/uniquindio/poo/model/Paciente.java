e36d4ff36c2ca13dedec2d3ed7e89e3180ba6e4c
JohnDehiby25 <dehibymorales@gmail.com>
2025-05-20 20:20:01 -0400

    Documentacion proyecto en clases, se realizo la documentacion o de todas las clases al inicio para informar de que va a tratar el proyecto con mi nombre y fechas

--- Code/proyectofinalhospital/src/main/java/co/edu/uniquindio/poo/model/Paciente.java
+++ Code/proyectofinalhospital/src/main/java/co/edu/uniquindio/poo/model/Paciente.java
@@ -1,3 +1,10 @@
+/* 
+ * Proyecto final de sistema de gestion de hospital
+ * 
+ * Integrantes
+ * John Dehiby Morales Guzmán
+ * Version: 1.0 Abril 2025
+ */
 package co.edu.uniquindio.poo.model;
 import java.util.ArrayList;
 import java.util.Scanner;

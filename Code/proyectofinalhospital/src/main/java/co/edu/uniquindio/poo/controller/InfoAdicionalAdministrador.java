92079d96af301e62637b746fb8330b40157096ae
JohnDehiby25 <dehibymorales@gmail.com>
2025-05-22 17:01:03 -0400

    Creacion de archivo fxml y controller infoAdicionalAdminstrador para utilizar otros metodos creados en el model

---
+++ Code/proyectofinalhospital/src/main/java/co/edu/uniquindio/poo/controller/InfoAdicionalAdministrador.java
@@ -0,0 +1,52 @@
+package  co.edu.uniquindio.poo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class InfoAdicionalAdministrador {
+
+    @FXML
+    private TextArea txtAreaCambioCitas;
+
+    @FXML
+    private TextArea txtAreaNotificaciones;
+
+    @FXML
+    private TextArea txtAreaReporteCitasMedicas;
+
+    @FXML
+    private TextArea txtAreaReporteOcupacionHospital;
+
+    @FXML
+    void cambiarCitas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiarCampos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notificarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void regresarVentanaPrincipal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportarCitas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportarOcupacion(ActionEvent event) {
+
+    }
+
+}
+
